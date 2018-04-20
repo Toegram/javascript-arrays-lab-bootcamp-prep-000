433aa3ebaaf2dfa2a197ec7cb8fd97bbaeaa3728
Anthony Turetsky <tturetsky@gmail.com>
2018-04-20 18:56:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrepedKitten(name) {
+  kittens.unshift(name);
+  return kittens;
+}
